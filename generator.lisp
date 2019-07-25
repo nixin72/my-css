f1c2f16b289e5d806b70cc915ba1267ce5a728a5
phdumaresq <phdumaresq@gmail.com>
2019-07-25 01:14:27 -0700

    Style changes from emacs

--- generator.lisp
+++ generator.lisp
@@ -2,8 +2,9 @@
 
 (defun add-class (name &rest props)
   (setf *css* (push (concatenate 'string "." name " { "
-      (join (loop for (k v) in props collect (concatenate 'string k ": " v "; ")) "")
-    " }" ) *css*)))
+                                 (join (loop
+                                         for (k v) in props collect (concatenate 'string k ": " v "; ")) "")
+                                 " }" ) *css*)))
 
 (defun join (lst delimiter)
   (let ((str ""))
@@ -16,25 +17,28 @@
         (t (append (flatten (car l)) (flatten (cdr l))))))
 
 (defvar sizes '((0 0)
-  (1 1)  (2 2)  (3 3)
-  (4 5)  (5 8)  (6 12)
-  (7 20) (8 32) (9 52)))
+                (1 1)  (2 2)  (3 3)
+                (4 5)  (5 8)  (6 12)
+                (7 20) (8 32) (9 52)))
 
 (defvar sides '((nil nil)
-  ("-top" "t") ("-right" "r")
-  ("-bottom" "b") ("-left" "l")))
+                ("-top" "t") ("-right" "r")
+                ("-bottom" "b") ("-left" "l")))
 
 (defvar properties '(("margin" "m")
-  ("padding" "p") ("border" "b")))
+                     ("padding" "p") ("border" "b")))
 
-(setf *css* (flatten (loop for (prop p) in properties collect
-  (loop for (side s) in sides collect
-    (loop for (i n) in sizes collect
-      (let ((s2 (write-to-string i)) (size (write-to-string n)))
-        (concatenate 'string
-          "." p s " { " prop side ": " size "; }")))))))
+(setf *css* (flatten
+             (loop for (prop p) in properties
+                   collect
+                   (loop for (side s) in sides
+                         collect
+                         (loop for (i n) in sizes
+                               collect
+                               (let ((s2 (write-to-string i)) (size (write-to-string n)))
+                                 (concatenate 'string
+                                              "." p s " { " prop side ": " size "; }")))))))
 
 (add-class "flex" ("display" "flex"))
 
 (print *css*)
-
